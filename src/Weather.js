fd8a00ad87b99a7e3e51ee7bf6b23eb3667c6168
camilaDhaier <violincamila@gmail.com>
2024-09-12 22:33:19 -0300

    trying to find the issue

--- src/Weather.js
+++ src/Weather.js
@@ -5,7 +5,8 @@ export default function Weather(props){
   function handleResponse(response){
     alert(`The weather in ${response.data.name} is ${response.data.main.temp} C`);
 
-  }
+}
+
   let apiKey = "fe1483f743b581b5520a1b725af03a49";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
